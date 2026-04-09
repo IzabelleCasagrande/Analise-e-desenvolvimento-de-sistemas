0401929d4138fe0773816c945f51754987e3ba6f
Izabelle Casagrande <contato.izabellecasagrande@gmail.com>
2026-04-09 03:42:10 -0300

    Resolução do hora de codar, aplicando os conhecimentos obtidos ate agora

--- 2026.1/Introducao-a-programação-de-computadores/Hora de codar/Nivel-Mestre/sistema_gerenciamento_simples_de_inventario.c
+++ 2026.1/Introducao-a-programação-de-computadores/Hora de codar/Nivel-Mestre/sistema_gerenciamento_simples_de_inventario.c
@@ -17,10 +17,19 @@ int main() {
     double valorTotalA;
     double valorTotalB;
 
+    int resultadoA, resultadoB;
+
     printf("Sistema de Gerenciamento Simples de Inventário\n\n");
     
-    printf("Produto: %s tem estoque: %u e o valor unitário é %.2f\n", produtoA, estoqueA, valorA);
-    printf("Produto: %s tem estoque: %u e o valor unitário é %.2f\n\n", produtoB, estoqueB, valorB);
+    printf("Produto: %s tem estoque: %u e o valor unitário é R$%.2f\n", produtoA, estoqueA, valorA);
+    printf("Produto: %s tem estoque: %u e o valor unitário é R$%.2f\n\n", produtoB, estoqueB, valorB);
 
+    resultadoA = estoqueA > estoqueMinimoA;
+    resultadoB = estoqueB > estoqueMinimoB;
+
+    printf("O produto %s tem estoque minimo %d\n",produtoA, resultadoA);
+    printf("O produto %s tem estoque minimo %d\n\n",produtoB, resultadoB);
+    
+    printf("Valor total de a (R$ %.2f) é maior que o valor total de b(R$ %.2f): %d\n", (estoqueA * valorA), (estoqueB * valorB), (estoqueA * valorA) > (estoqueB * valorB));
     return 0;
 }
